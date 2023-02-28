95a716eb72c70791d767103f40670e31d5452f4e
IrinaVaronka <varonkairina@gmail.com>
2023-02-28 14:41:02 +0200

    php-008

---
+++ 008/index.php
@@ -0,0 +1,52 @@
+
+    <?php
+       $labas = 5;
+
+       //echo $labas++; 
+
+        echo '<br>';
+
+        echo $labas++ + ++$labas;
+
+        
+        $pirmas = 'bla bla';
+        $antras = 'ku ku';
+        $trecias = $pirmas . $antras;
+
+        echo '<br>';
+
+        echo $trecias;
+
+        echo '<br>';
+
+        $trecias2 = null;
+
+        var_dump(isset($trecias2));
+        echo '<br>';
+
+        var_dump($trecias2 === null);
+
+        $vienas = 'a';
+
+        $a = 'b';
+
+        $b = 'c';
+
+        echo '<br>';
+        echo $$$vienas;
+
+        echo '<pre>';
+
+        print_r([1, 2, 3, 5]);
+       
+        $pirmas = 'bla bla';
+        $antras = "ku $pirmas ku";
+        
+
+        echo '<br>';
+        echo $antras;
+        
+
+
+
+    
