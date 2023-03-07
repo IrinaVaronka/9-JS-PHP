b1de131a808a584c51656595a8b628c2f1cf3d39
IrinaVaronka <varonkairina@gmail.com>
2023-03-07 09:49:24 +0200

    file save

--- 011/data/index.php
+++ 011/data/index.php
@@ -1,10 +1,28 @@
 <?php
 
 
-file_put_contents(__DIR__ . '/labas.txt', 'labas!');
+
+//file_put_contents(__DIR__ . '/labas.txt', 'labas!');
 
 //file_put_contents(__DIR__ . '/labas.txt', "\nRACOON", FILE_APPEND);
 
 // echo '<pre>';
 
 // echo file_get_contents(__DIR__ . '/labas.txt');
+
+//SEREALIZE
+
+if (!file_exists(__DIR__ . '/data.ser')) {
+    $data = [];
+} else {
+    $data = file_get_contents(__DIR__ . '/data.ser');
+    $data = unserialize($data);
+}
+
+echo '<pre>';
+print_r($data);
+
+$data[] = ['number' => rand(100, 999), 'color' => rand(0, 1) ? 'red' : 'skyblue'];
+
+$data = json_encode($data);
+$data = file_put_contents(__DIR__ . '/data/ser');
