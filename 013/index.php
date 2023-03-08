aba01ae45bf36c9eea32a48b338f95ad2a925841
IrinaVaronka <varonkairina@gmail.com>
2023-03-08 10:20:37 +0200

    post

---
+++ 013/index.php
@@ -0,0 +1,46 @@
+<?php
+
+echo '<pre>';
+//POST
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // kazka padarom
+
+        echo '<pre>';
+        print_r($_POST);
+        die;
+
+        header('location: http://localhost/9-JS-PHP/013/');  //redirect
+        die; // script done work
+
+}
+
+//GET scenarijus
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>013</h1>
+    <form action="" method="post">
+        <input type="text" name="animal">
+        <br>
+        <input type="range" min="0" max="10" value="0" name="magic_number">
+        <br>
+        <button type="submit">GO</button>
+    </form>
+</body>
+
+</html>
