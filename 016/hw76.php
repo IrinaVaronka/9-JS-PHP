397a25d69b862e7eb2b8e8cccabbb4d18730e419
IrinaVaronka <varonkairina@gmail.com>
2023-03-14 11:38:30 +0200

    hw

---
+++ 016/hw76.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $_SESSION['color'] = 1;
+    header('Location: http://localhost/9-JS-PHP/016/hw76.php');
+    die;
+}
+
+if (isset($_SESSION['color'])) {
+    $color = 'yellow';
+    unset($_SESSION['color']);
+} else {
+    $color = 'green';
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>doc</title>
+</head>
+
+<body style="background: <?= $color ?>;">
+<form method= "get">
+    <button type="submit">GET</button>
+</form> 
+
+<form method= "post">
+    <button type="submit">POST</button>
+</form> 
+
+</body>
+</html>
